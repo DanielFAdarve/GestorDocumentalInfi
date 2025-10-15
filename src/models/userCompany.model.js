8d4793a17f7d72364322f2946674387a1861e759
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-10-14 23:47:58 -0500

    Update. Se ajusta el userCompany para integrar enum de la area

--- src/models/userCompany.model.js
+++ src/models/userCompany.model.js
@@ -2,7 +2,21 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
 const UserCompany = sequelize.define("UserCompany", {
-  id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true }
+  id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
+  userId: { type: DataTypes.BIGINT, allowNull: false },
+  companyId: { type: DataTypes.BIGINT, allowNull: false },
+  area: {
+    type: DataTypes.ENUM(
+      "administrativa",
+      "técnica",
+      "financiera",
+      "contable",
+      "legal",
+      "no_determinada"
+    ),
+    allowNull: false,
+    defaultValue: "no_determinada"
+  }
 }, {
   tableName: "users_companies",
   timestamps: false
