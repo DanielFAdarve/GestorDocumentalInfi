a18e64f06bc98f6fb06e39ea081acfa4f3d3091d
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-10-17 23:56:13 -0500

    refactor support model

--- src/models/support.model.js
+++ src/models/support.model.js
@@ -1,14 +1,17 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+import { DataTypes, Model } from "sequelize";
 
-const Support = sequelize.define("Support", {
-  id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
-  support_name: { type: DataTypes.STRING, allowNull: false },
-  description: { type: DataTypes.STRING },
-  delivery_term: { type: DataTypes.BIGINT }
-}, {
-  tableName: "supports",
-  timestamps: false
-});
+export default (sequelize) => {
+  class Support extends Model {}
 
-module.exports = Support;
+  Support.init(
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      support_name: { type: DataTypes.STRING, allowNull: false },
+      description: { type: DataTypes.STRING },
+      delivery_term: { type: DataTypes.INTEGER },
+    },
+    { sequelize, modelName: "Support", tableName: "supports", timestamps: true }
+  );
+
+  return Support;
+};
