229284255c0e5ddf0f7964d9646ac47622abba7f
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-10-20 23:49:20 -0500

    feat se agrega al index la ruta de company y se hace la inyeccion de dependencias

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+const JwtHelper = require('../utils/jwt');
 
-const { User } = require('../database'); // o donde definas el modelo
+const { User,Company } = require('../database'); 
+
+// Importar repositorios y servicios
 const UserRepository = require('../modules/user/user.repository');
 const UserService = require('../modules/user/user.service');
-const JwtHelper = require('../utils/jwt');
+
+const CompanyRepository = require('../modules/company/company.repository');
+const CompanyService = require('../modules/company/company.service');
 
 
 // Importar rutas por módulo
 const userRoutes = require('../modules/user/user.routes');
-// const companyRoutes = require('../modules/company/company.routes');
+const companyRoutes = require('../modules/company/company.routes');
 // const contractRoutes = require('../modules/contract/contract.routes');
 
 
-// Inicializar dependencias (inyección manual)
+
+// Instancias e inyección de dependencias
+//User
 const userRepository = new UserRepository(User);
 const userService = new UserService(userRepository);
+
+//Company
+const companyRepository = new CompanyRepository(Company);
+const companyService = new CompanyService(companyRepository);
 
 
 // Registrar rutas base
 router.use('/users', userRoutes(userService, JwtHelper));
-// router.use('/companies', companyRoutes);
+router.use('/companies', companyRoutes(companyService));
 // router.use('/contracts', contractRoutes);
 
 module.exports = router;
