042e0870471f522b373b37ed5963119806464da0
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-10-20 23:57:39 -0500

    update contract controller se ajusta a la arquitectura

--- src/modules/contract/contract.controller.js
+++ src/modules/contract/contract.controller.js
@@ -1,35 +1,56 @@
-const ContractService = require("./contract.service");
-const Response = require("../models/Response.model");
+const { createContractSchema, updateContractSchema } = require('./contract.schema');
 
 class ContractController {
-  async create(req, res) {
-    const result = await ContractService.createContract(req.body);
-    if (!result.success) return res.status(400).json(Response.set(400, false, result.message));
-    return res.json(Response.set(201, true, result.data));
+  constructor(contractService) {
+    this.contractService = contractService;
   }
 
-  async getAll(req, res) {
-    const contracts = await ContractService.getAllContracts();
-    return res.json(Response.set(contracts));
-  }
+  getAll = async (req, res, next) => {
+    try {
+      const contracts = await this.contractService.getAllContracts();
+      res.status(200).json(contracts);
+    } catch (err) {
+      next(err);
+    }
+  };
 
-  async getById(req, res) {
-    const result = await ContractService.getContractById(req.params.id);
-    if (!result.success) return res.status(404).json(Response.set(404, false, result.message));
-    return res.json(Response.set(result.data));
-  }
+  getById = async (req, res, next) => {
+    try {
+      const contract = await this.contractService.getContractById(req.params.id);
+      res.status(200).json(contract);
+    } catch (err) {
+      next(err);
+    }
+  };
 
-  async update(req, res) {
-    const result = await ContractService.updateContract(req.params.id, req.body);
-    if (!result.success) return res.status(400).json(Response.set(400, false, result.message));
-    return res.json(Response.set(200, true, result.data));
-  }
+  create = async (req, res, next) => {
+    try {
+      const validated = createContractSchema.parse(req.body);
+      const contract = await this.contractService.createContract(validated);
+      res.status(201).json(contract);
+    } catch (err) {
+      next(err);
+    }
+  };
 
-  async delete(req, res) {
-    const result = await ContractService.deleteContract(req.params.id);
-    if (!result.success) return res.status(404).json(Response.set(404, false, result.message));
-    return res.json(Response.set(200, true, result.message));
-  }
+  update = async (req, res, next) => {
+    try {
+      const validated = updateContractSchema.parse(req.body);
+      const contract = await this.contractService.updateContract(req.params.id, validated);
+      res.status(200).json(contract);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  delete = async (req, res, next) => {
+    try {
+      const result = await this.contractService.deleteContract(req.params.id);
+      res.status(200).json(result);
+    } catch (err) {
+      next(err);
+    }
+  };
 }
 
-module.exports = new ContractController();
+module.exports = ContractController;
