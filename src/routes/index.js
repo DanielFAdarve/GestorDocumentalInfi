1f4ab48ecc4e217f631860c90b5299bd7f28302c
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-10-21 23:44:37 -0500

    upda se actualiza imports

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const JwtHelper = require('../utils/jwt');
 
-const { User,Company } = require('../database'); 
+const models = require('../database'); 
 
 // Importar repositorios y servicios
 const UserRepository = require('../modules/user/user.repository');
@@ -12,27 +12,32 @@ const UserService = require('../modules/user/user.service');
 const CompanyRepository = require('../modules/company/company.repository');
 const CompanyService = require('../modules/company/company.service');
 
+const ContractRepository = require('../modules/contract/contract.repository');
+const ContractService = require('../modules/contract/contract.service');
 
 // Importar rutas por módulo
 const userRoutes = require('../modules/user/user.routes');
 const companyRoutes = require('../modules/company/company.routes');
-// const contractRoutes = require('../modules/contract/contract.routes');
+const contractRoutes = require('../modules/contract/contract.routes');
 
 
 
 // Instancias e inyección de dependencias
 //User
-const userRepository = new UserRepository(User);
+const userRepository = new UserRepository(models.User);
 const userService = new UserService(userRepository);
 
 //Company
-const companyRepository = new CompanyRepository(Company);
+const companyRepository = new CompanyRepository(models.Company);
 const companyService = new CompanyService(companyRepository);
 
+//Contract
+const contractRepository = new ContractRepository(models.Contract, models.UserContractRole);
+const contractService = new ContractService(contractRepository, models.sequelize);
 
 // Registrar rutas base
 router.use('/users', userRoutes(userService, JwtHelper));
 router.use('/companies', companyRoutes(companyService));
-// router.use('/contracts', contractRoutes);
+router.use('/contracts',  contractRoutes(contractService));
 
 module.exports = router;
