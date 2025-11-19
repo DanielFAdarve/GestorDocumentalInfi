a2d87d8ded09b1deed896be7effaf97daf3c7a17
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-11-18 23:20:56 -0500

    Update role.model.js

--- src/database/models/role.model.js
+++ src/database/models/role.model.js
@@ -3,19 +3,19 @@ const { Model, DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   class Role extends Model {
     static associate(models) {
-      Role.hasMany(models.UserCompanyRole, { foreignKey: 'roleId' });
-      Role.hasMany(models.UserContractRole, { foreignKey: 'roleId' });
+      Role.hasMany(models.UserCompanyRole, { foreignKey: 'role_id' });
+      Role.hasMany(models.UserContractRole, { foreignKey: 'role_id' });
     }
   }
 
-  Role.init(
-    {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      name: { type: DataTypes.STRING, allowNull: false },
-      description: { type: DataTypes.TEXT },
-    },
-    { sequelize, modelName: 'Role', tableName: 'roles', timestamps: true }
-  );
+  Role.init({
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: true }
+  }, {
+    sequelize, modelName: 'Role', tableName: 'roles', timestamps: true, underscored: true,
+    indexes: [{ fields: ['name'] }]
+  });
 
   return Role;
 };
